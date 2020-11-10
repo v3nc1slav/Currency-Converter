c86965e3af50c44c8bb79ddff1b12f55db02285e
v3nc1slav <v.verchov@mail.bg>
2020-11-10 23:05:52 +0200

    add Views and Controllers for More Options and Server

---
+++ Currency converter C#/Currency converter/Controllers/MoreOptionsController.cs
@@ -0,0 +1,34 @@
+﻿namespace Currency_converter.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Currency_converter.Models;
+
+    public class MoreOptionsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public MoreOptionsController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> MoreOptions()
+        {
+            _logger.LogInformation("Get MoreOptions");
+            return this.View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public async Task<IActionResult> Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Currency converter C#/Currency converter/Controllers/ServicesController.cs
@@ -0,0 +1,35 @@
+﻿namespace Currency_converter.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Currency_converter.Models;
+
+    public class ServicesController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ServicesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Services()
+        {
+            _logger.LogInformation("Get Services");
+
+            return this.View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public async Task<IActionResult> Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
